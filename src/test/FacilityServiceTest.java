81fa919087fc87e49134a05c4981328ba2cfcfc1
uchennafokoye <okoyefaithful@gmail.com>
2016-04-29 00:10:20 -0500

    First test case

---
+++ src/test/FacilityServiceTest.java
@@ -0,0 +1,24 @@
+package test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by uchennafokoye on 4/28/16.
+ */
+public class FacilityServiceTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getInstance() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getFacility() throws Exception {
+
+    }
+}
---
+++ src/test/facilityservice/FacilityServiceTest.java
@@ -0,0 +1,52 @@
+package test.facilityservice;
+
+
+import logistics.facilityservice.FacilityDTO;
+import logistics.facilityservice.FacilityService;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by uchennafokoye on 4/28/16.
+ */
+
+public class FacilityServiceTest {
+
+    FacilityService instance = null;
+
+    @Before
+    public void setUp()  {
+         instance = FacilityService.getInstance();
+    }
+
+    @Test
+    public void getInstance() {
+        assertNotNull(FacilityService.getInstance());
+    }
+
+    @Test
+    public void getFacilityName() {
+        assertNotNull(FacilityService.getInstance());
+        FacilityDTO facility = instance.getFacility("San Francisco, CA");
+        assertEquals("San Francisco, CA", facility.name);
+    }
+
+    @Test
+    public void getFacilityCost() {
+        assertNotNull(FacilityService.getInstance());
+        FacilityDTO facility = instance.getFacility("San Francisco, CA");
+        assertEquals(facility.cost, 300, 0.002);
+    }
+
+    @Test
+    public void getFacilityNull() {
+        assertNotNull(FacilityService.getInstance());
+        FacilityDTO facility = instance.getFacility(null);
+        assertNull(facility);
+    }
+
+
+
+}
