8e847c357330f7ae2935fd7131ffb3cea41967ee
soul-mide <crystilis@gmail.com>
2016-05-05 12:35:22 -0500

    added comments to InventoryItemDTO.java

--- src/logistics/inventoryservice/inventoryitem/InventoryItemDTO.java
+++ src/logistics/inventoryservice/inventoryitem/InventoryItemDTO.java
@@ -1,6 +1,10 @@
 package logistics.inventoryservice.inventoryitem;
 
 /**
+ * This class represents an Inventory-item Data Transfer Object,
+ * which is used by the Inventory Service manager when communicating
+ * with other subsystems of the Logistics application.
+ * 
  * @author David Olorundare
  */
 public class InventoryItemDTO
@@ -8,7 +12,11 @@ public class InventoryItemDTO
 
     public String id;
     public int quantity;
-
+    
+    /*
+     * Constructor that creates a new Inventory-item DTO given an 
+     * item's ID and quantity.
+     */
     public InventoryItemDTO(String itemId, int itemQuantity)
     {
         id = itemId;
