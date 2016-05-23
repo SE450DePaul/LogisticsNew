05b17f46091665587452e533ac3fea84a6c7b3da
soul-mide <crystilis@gmail.com>
2016-05-22 20:05:09 -0500

    added comments to LoaderConfig.java

--- src/logistics/utilities/loader/LoaderConfig.java
+++ src/logistics/utilities/loader/LoaderConfig.java
@@ -1,10 +1,13 @@
 package logistics.utilities.loader;
 
 /**
- * Created by uchennafokoye on 4/28/16.
+ * This class represents a configuration file containing all the
+ * filesystem paths to the various XML data that will be loaded into
+ * the Logistic application.
+ * 
+ * @author Uchenna F. Okoye
  */
 public class LoaderConfig {
-
 
     public static final class FilePath {
 
@@ -14,8 +17,5 @@ public class LoaderConfig {
         public static final String NETWORK = "data/facility_network.xml";
         public static final String ORDERS = "data/orders.xml";
         public static final String INVENTORY = "data/facility_inventory.xml";
-
     }
-
-
 }
